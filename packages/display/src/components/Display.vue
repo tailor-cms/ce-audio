f8503e31c455542b3fbd5c91eb0037012b454314
rkusan00 <renato.kusanovic@gmail.com>
2025-07-09 12:00:07 +0200

    Migrate Display component

--- packages/display/src/components/Display.vue
+++ packages/display/src/components/Display.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vuejs-accessibility/media-has-caption -->
 <template>
-  <div v-if="data.url" class="tce-root">
+  <div v-if="data.url" class="tce-audio-root">
     <audio :src="data.url" class="w-100" controls @play="submit"></audio>
   </div>
 </template>
@@ -13,10 +13,3 @@ const emit = defineEmits(['interaction']);
 
 const submit = () => emit('interaction', { id: props.id });
 </script>
-
-<style scoped>
-.tce-root {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 1rem;
-}
-</style>
