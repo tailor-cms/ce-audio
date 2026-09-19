cbc0ac4264e31be0c49cad0c79a4c0b55d2fa8bd
rkusan00 <renato.kusanovic@gmail.com>
2026-09-19 23:57:19 +0200

    Move upload to main element dropzone

--- packages/edit/src/components/Edit.vue
+++ packages/edit/src/components/Edit.vue
@@ -1,33 +1,56 @@
 <!-- eslint-disable vuejs-accessibility/media-has-caption -->
 <template>
-  <div class="tce-audio">
+  <div class="tce-audio text-left">
     <TailorElementPlaceholder
-      v-if="!element.data.url"
+      v-if="!element.data.url && isReadonly"
       :icon="manifest.ui.icon"
-      :is-disabled="isReadonly"
-      :is-focused="isFocused"
       :name="`${manifest.name} component`"
-      active-icon="mdi-arrow-up"
-      active-placeholder="Use toolbar to upload the audio file"
+      is-readonly
     />
-    <audio v-else :src="element.data.url" class="w-100" controls></audio>
+    <TailorFileInput
+      v-else
+      :allowed-extensions="EXTENSIONS"
+      :file-key="element.data.assets?.url || element.data.url"
+      :public-url="element.data.url"
+      :readonly="isReadonly"
+      :show-actions="isFocused"
+      mode="dropzone"
+      allow-url-source
+      @delete="onDelete"
+      @input="save"
+      @upload="save"
+    >
+      <audio
+        :src="element.data.url ?? ''"
+        class="d-block w-100"
+        controls
+      ></audio>
+    </TailorFileInput>
   </div>
 </template>
 
 <script lang="ts" setup>
-import type { Element } from '@tailor-cms/ce-audio-manifest';
+import type { Element, ElementData } from '@tailor-cms/ce-audio-manifest';
 import manifest from '@tailor-cms/ce-audio-manifest';
 
-defineProps<{
+const EXTENSIONS = ['.mp3', '.aac', '.ogg', '.wma', '.flac', '.m4a', '.wav'];
+
+const props = defineProps<{
   element: Element;
   isDragged: boolean;
   isFocused: boolean;
   isReadonly: boolean;
 }>();
-</script>
+const emit = defineEmits<{ save: [data: ElementData] }>();
 
-<style lang="scss" scoped>
-.tce-audio {
-  text-align: left;
-}
-</style>
+const save = (payload: Record<string, any> | null) => {
+  if (!payload) return;
+  const { url, publicUrl } = payload;
+  const assets = { url };
+  emit('save', { ...props.element.data, url: publicUrl ?? url, assets });
+};
+
+const onDelete = () => {
+  emit('save', { ...props.element.data, url: null, assets: {} });
+};
+</script>
--- packages/edit/src/index.ts
+++ packages/edit/src/index.ts
@@ -2,13 +2,11 @@ import baseManifest from '@tailor-cms/ce-audio-manifest';
 import type { ElementManifest } from '@tailor-cms/ce-audio-manifest';
 
 import Edit from './components/Edit.vue';
-import TopToolbar from './components/TopToolbar.vue';
 
 const manifest: ElementManifest = {
   ...baseManifest,
   Edit,
-  TopToolbar,
 };
 
 export default manifest;
-export { Edit, TopToolbar };
+export { Edit };
